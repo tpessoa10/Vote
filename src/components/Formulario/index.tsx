27bcbfa6f934a7754c89cb551dc20173dfbdfa01
tpessoa10 <48775460+tpessoa10@users.noreply.github.com>
2023-09-29 21:50:23 -0300

    Update index.tsx

--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -50,7 +50,7 @@ export default function Formulario(){
 
         console.log(data)
 
-        fetch("https://json-server-wheat-two.vercel.app/topicos",{
+        fetch("http://localhost:3000/topicos",{
             method:"POST",
             headers:{
                 'Content-type':'application/json'
